163d8449e6841fddf2862c3219857ad09b77a09c
Lucia Castillo <111028118+dalu09@users.noreply.github.com>
2024-05-05 23:39:20 -0500

    App vacia

---
+++ mynewapp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SerieComponent } from './serie/serie.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+      SerieComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
